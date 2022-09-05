77ccc64dfa46837b6c402bc5a2c3df5c6f49b594
MIKE <mikebonus777@gmail.com>
2022-09-05 15:06:24 -0400

    Added ExpressionParser.kt

---
+++ app/src/test/java/com/example/bitrise/domain/ExpressionParserTest.kt
@@ -0,0 +1,56 @@
+package com.example.bitrise.domain
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class ExpressionParserTest {
+
+    private lateinit var parser: ExpressionParser
+
+    @Test
+    fun `Simple expression is properly parsed`() {
+
+        // 1. GIVEN
+        parser = ExpressionParser("3+5-3*4/3")
+
+        // 2. DO SOMETHING WITH WHAT IS GIVEN
+        val actual = parser.parse()
+
+        // 3. ASSERT EXPECTED == ACTUAL VALUE
+        val expected = listOf(
+            ExpressionPart.Number(3.0),
+            ExpressionPart.Op(Operation.ADD),
+            ExpressionPart.Number(5.0),
+            ExpressionPart.Op(Operation.SUBTRACT),
+            ExpressionPart.Number(3.0),
+            ExpressionPart.Op(Operation.MULTIPLY),
+            ExpressionPart.Number(4.0),
+            ExpressionPart.Op(Operation.DIVIDE),
+            ExpressionPart.Number(3.0)
+        )
+
+        assertThat(expected).isEqualTo(actual)
+
+    }
+
+    @Test
+    fun `Expression with parentheses is properly parsed`() {
+
+        parser = ExpressionParser("4-(4*5)")
+
+        val actual = parser.parse()
+
+        val expected = listOf(
+            ExpressionPart.Number(4.0),
+            ExpressionPart.Op(Operation.SUBTRACT),
+            ExpressionPart.Parentheses(ParenthesesType.Opening),
+            ExpressionPart.Number(4.0),
+            ExpressionPart.Op(Operation.MULTIPLY),
+            ExpressionPart.Number(5.0),
+            ExpressionPart.Parentheses(ParenthesesType.Closing),
+        )
+
+        assertThat(expected).isEqualTo(actual)
+    }
+
+}
